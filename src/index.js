bcaa0e06b31849def37ba67c560028dca8b686a2
Waseem Maya <waseemmayaa@gmail.com>
2018-09-04 20:56:19 +0500

    push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Component/Assignment-1';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
